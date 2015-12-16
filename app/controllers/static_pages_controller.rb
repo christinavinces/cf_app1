311d9b8f50c871f17946e9101d3989dc5e361be5
Christina Vinces <christina.vinces@gmail.com>
2015-12-16 18:06:28 -0500

    add email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
   def index
   end
+
+  def landing_page
+  	@featured_product = Product.first
+  	@products = Product.all
+  end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  	    :to => 'christina.vinces@gmail.com',
+  	    :subject => "A new contact form message from #{@name}",
+  	    :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
+
+  post 'static_pages/thank_you'
+  
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
